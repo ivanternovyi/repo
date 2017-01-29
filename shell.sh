9ae62be5845c664f047e620f2cb88e4e7575c275
nk_hard <ivanternovyi@gmail.com>
2017-01-29 15:55:47 +0200

    Add files via upload

---
+++ shell.sh
@@ -0,0 +1,7 @@
+#/bin/bash	
+#shell_script
+
+echo Script has started!
+atom
+firefox https://vk.com/nk_hard https://translate.google.com.ua/?hl=uk&tab=wT
+evince Desktop/programming-ruby-1-9-and-2-0.pdf & exit
